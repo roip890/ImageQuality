abc488676198cb469bebf0be828ef687cefc408a
roip890 <roip890@gmail.com>
2021-01-18 00:53:09 +0200

    data sampling

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from data import Data
 import matplotlib.pyplot as plt
@@ -9,26 +10,48 @@ class App(object):
     def __init__(self):
         self.data = Data()
 
+    @staticmethod
+    def plot(y_values, x_values, y_label, x_label):
+        plt.plot(x_values, y_values)
+        plt.ylabel(y_label)
+        plt.xlabel(x_label)
+        plt.show()
+
+    def sample_data(self, experts, num_iterations=50, num_batch=1000):
+        samples_averages = []
+        iterations = range(1, num_iterations+1)
+        for i in range(1, num_iterations+1):
+            samples = [self.data.sample_data(samples=i, experts=experts)[0] for j in range(num_batch)]
+            samples_averages.append(sum(samples) / len(samples))
+            print('Iteration Num: ' + str(i))
+        return samples_averages, iterations
+
 
 def start(args):
 
     # init app
     app = App()
 
-    # run first experiment
-    accuracy, distribution, samples = app.data.sample_data(
-        samples=500,
-        experiment=Experiment.All,
-        experts=Experts.All,
-        distributions={
-           Experts.Beginner: 0.2,
-           Experts.Medium: 0.3,
-           Experts.Expert: 0.5
-        })
+    # beginner
+    print('Start Beginner:')
+    samples_averages, iterations = app.sample_data(Experts.Beginner, num_iterations=20, num_batch=10000)
+    app.plot(samples_averages, iterations, 'Samples Averages', 'Iterations')
+    print('Beginner Samples Averages:' + str([round(value, 3) for value in samples_averages]))
+    print('Expert End Beginner:')
 
-    print('Accuracy: ' + str(accuracy))
-    print('Distribution: ' + str(distribution))
-    print('Samples: ' + str(samples))
+    # medium
+    print('Start Medium:')
+    samples_averages, iterations = app.sample_data(Experts.Medium, num_iterations=20, num_batch=10000)
+    app.plot(samples_averages, iterations, 'Samples Averages', 'Iterations')
+    print('Medium Samples Averages:' + str([round(value, 3) for value in samples_averages]))
+    print('End Medium:')
+
+    # expert
+    print('Start Expert:')
+    samples_averages, iterations = app.sample_data(Experts.Expert, num_iterations=20, num_batch=10000)
+    app.plot(samples_averages, iterations, 'Samples Averages', 'Iterations')
+    print('Expert Samples Averages:' + str([round(value, 3) for value in samples_averages]))
+    print('End Expert:')
 
 
 start(sys.argv)
--- data.py
+++ data.py
@@ -22,7 +22,7 @@ class Data(object):
     def generate_dataframe(self, filename):
         return pd.read_csv(filename, index_col=0).iloc[:, 2:].replace(True, 1).replace(False, 0)
 
-    def sample_data(self, samples, experiment, experts, distributions):
+    def sample_data(self, samples, experts, experiment=Experiment.All, distributions=None):
 
         # experiment
         if experiment == Experiment.All:
@@ -43,30 +43,47 @@ class Data(object):
         samples = min(samples, len(experiment_range) * len(experts_range) * len(users_range))
 
         # indices
-        if distributions == None:
+        if distributions is None:
             sample_indices = random.sample(
                 [(i, j, k) for i in experts_range for j in users_range for k in experiment_range], k=samples)
         else:
             sample_indices = []
-            beginner_samples = min(int(samples * distributions.get(Experts.Beginner, 0)), len(experiment_range) * len(users_range))
+            beginner_samples = min(int(samples * distributions.get(Experts.Beginner, 0)),
+                                   len(experiment_range) * len(users_range))
             sample_indices.extend(random.sample(
-                [(i, j, k) for i in [Experts.Beginner.value] for j in users_range for k in experiment_range], k=beginner_samples))
-            medium_samples = min(int(samples * distributions.get(Experts.Medium, 0)), len(experiment_range) * len(users_range))
+                [(i, j, k) for i in [Experts.Beginner.value] for j in users_range for k in experiment_range],
+                k=beginner_samples))
+            medium_samples = min(int(samples * distributions.get(Experts.Medium, 0)),
+                                 len(experiment_range) * len(users_range))
             sample_indices.extend(random.sample(
-                [(i, j, k) for i in [Experts.Medium.value] for j in users_range for k in experiment_range], k=medium_samples))
-            expert_samples = min(int(samples * distributions.get(Experts.Expert, 0)), len(experiment_range) * len(users_range))
+                [(i, j, k) for i in [Experts.Medium.value] for j in users_range for k in experiment_range],
+                k=medium_samples))
+            expert_samples = min(int(samples * distributions.get(Experts.Expert, 0)),
+                                 len(experiment_range) * len(users_range))
             sample_indices.extend(random.sample(
-                [(i, j, k) for i in [Experts.Expert.value] for j in users_range for k in experiment_range], k=expert_samples))
+                [(i, j, k) for i in [Experts.Expert.value] for j in users_range for k in experiment_range],
+                k=expert_samples))
             samples = beginner_samples + medium_samples + expert_samples
         # sample data
         data = [(sample[0], self.data[sample[0]].iloc[sample[1]][sample[2]]) for sample in sample_indices]
 
         # return calculations
-        return self.calculate_accuracy(data), self.group_data(data), samples
+        return self.calculate_most_accuracy(data), self.group_data(data), samples
 
     @staticmethod
     def calculate_accuracy(data):
         return sum([value[1] for value in data]) / len(data)
+
+    @staticmethod
+    def calculate_most_accuracy(data):
+        ones_len = len([value[1] for value in data if value[1] == 1])
+        zeros_len = len([value[1] for value in data if value[1] == 0])
+        if ones_len > zeros_len:
+            return 1
+        elif zeros_len > ones_len:
+            return 0
+        else:
+            return random.randint(0, 1)
 
     @staticmethod
     def group_data(data):
