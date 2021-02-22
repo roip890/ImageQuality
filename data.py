f6e1f2e1e72b1171d9b9c1a62c8d3deb3f8e1d4f
Roi Peretz <roip890@gmail.com>
2021-02-22 20:30:49 +0200

    init

--- data.py
+++ data.py
@@ -22,13 +22,13 @@ class Data(object):
     def generate_dataframe(self, filename):
         return pd.read_csv(filename, index_col=0).iloc[:, 2:].replace(True, 1).replace(False, 0)
 
-    def sample_data(self, samples, experts=Experts.All, experiment=Experiment.Random, distributions=None):
+    def sample_data(self, samples, experts=Experts.All, experiment=Experiment.Random, distributions=None, accuracy_function=None):
 
         # experiment
         if experiment == Experiment.All:
             experiment_range = range(self.num_experiments)
         elif experiment == Experiment.Random:
-            experiment_range = [random.randint(0, self.num_experiments-1)]
+            experiment_range = [random.randint(0, self.num_experiments - 1)]
         else:
             experiment_range = [experiment.value]
 
@@ -71,8 +71,11 @@ class Data(object):
         # sample data
         data = [(sample[0], self.data[sample[0]].iloc[sample[1]][sample[2]]) for sample in sample_indices]
 
+        # accuracy function
+        if accuracy_function is None:
+            accuracy_function = self.calculate_most_accuracy
         # return calculations
-        return self.calculate_most_accuracy(data), self.group_data(data), samples
+        return accuracy_function(data), self.group_data(data), samples
 
     @staticmethod
     def calculate_mean_accuracy(data):
@@ -88,6 +91,77 @@ class Data(object):
             return 0
         else:
             return random.randint(0, 1)
+
+    @staticmethod
+    def calculate_weighted_most_accuracy(data):
+        # beginners
+        beginners_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Beginner.value])
+        beginners_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Beginner.value])
+
+        # mediums
+        mediums_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Medium.value])
+        mediums_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Medium.value])
+
+        # experts
+        experts_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Expert.value])
+        experts_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Expert.value])
+
+        # ones
+        ones_len = (beginners_ones_len * Experts.expert_price(Experts.Beginner)) + (mediums_ones_len * Experts.expert_price(Experts.Medium)) + (experts_ones_len * Experts.expert_price(Experts.Expert))
+        # zeros
+        zeros_len = (beginners_zeros_len * Experts.expert_price(Experts.Beginner)) + (mediums_zeros_len * Experts.expert_price(Experts.Medium)) + (experts_zeros_len * Experts.expert_price(Experts.Expert))
+
+        if ones_len > zeros_len:
+            return 1
+        elif zeros_len > ones_len:
+            return 0
+        else:
+            return random.randint(0, 1)
+
+    @staticmethod
+    def calculate_most_accuracy_with_tie_breaker(tie_breaker):
+        def calculate_most_accuracy_with_tie_breaker_helper(data):
+            ones_len = len([value[1] for value in data if value[1] == 1])
+            zeros_len = len([value[1] for value in data if value[1] == 0])
+            if ones_len > zeros_len:
+                return 1
+            elif zeros_len > ones_len:
+                return 0
+            else:
+                if tie_breaker == Experts.Beginner:
+                    # beginners
+                    beginners_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Beginner.value])
+                    beginners_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Beginner.value])
+                    if beginners_ones_len > beginners_zeros_len:
+                        return 1
+                    elif beginners_zeros_len > beginners_ones_len:
+                        return 0
+                    else:
+                        return random.randint(0, 1)
+                elif tie_breaker == Experts.Medium:
+                    # mediums
+                    mediums_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Medium.value])
+                    mediums_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Medium.value])
+                    if mediums_ones_len > mediums_zeros_len:
+                        return 1
+                    elif mediums_zeros_len > mediums_ones_len:
+                        return 0
+                    else:
+                        return random.randint(0, 1)
+                elif tie_breaker == Experts.Expert:
+                    # experts
+                    experts_ones_len = len([value[1] for value in data if value[1] == 1 and value[0] == Experts.Expert.value])
+                    experts_zeros_len = len([value[1] for value in data if value[1] == 0 and value[0] == Experts.Expert.value])
+                    if experts_ones_len > experts_zeros_len:
+                        return 1
+                    elif experts_zeros_len > experts_ones_len:
+                        return 0
+                    else:
+                        return random.randint(0, 1)
+                else:
+                    return random.randint(0, 1)
+
+        return calculate_most_accuracy_with_tie_breaker_helper
 
     @staticmethod
     def group_data(data):
